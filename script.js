63e559f5cd96e6d1ab359e032174755d25d6c801
EgorKravetz <kravetzegor@gmail.com>
2021-11-12 21:14:46 +0300

    Add case-insensitive parameter for playerSelection

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const computerPlay = function() {
-  const arr = ['Rock','Paper','Scissors'];
+  const arr = ['rock','paper','scissors'];
   return arr[Math.floor(Math.random() * 3)];
 }
 
 const playRound = function(playerSelection, computerSelection) {
   if (playerSelection == null) {
-    return `You canceled game`;}
-  else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-    return `You win, ${playerSelection} beat ${computerSelection}`;}
-  else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-    return `You win, ${playerSelection} beat ${computerSelection}`;}
-  else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-    return `You win, ${playerSelection} beat ${computerSelection}`;}
+    return `You canceled game`}
+  else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return `You win, ${playerSelection} beat ${computerSelection}`}
+  else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return `You win, ${playerSelection} beat ${computerSelection}`}
+  else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return `You win, ${playerSelection} beat ${computerSelection}`}
   else if (playerSelection == computerSelection) {
-    return `Draw ps ${playerSelection} and cs ${computerSelection}`;}
+    return `Draw ps ${playerSelection} and cs ${computerSelection}`}
     return `You lose, ${computerSelection} beat ${playerSelection}`;
    }
 
-const playerSelection = prompt('Enter word');
+const playerPrompt = prompt('Enter word');
+const playerSelection = playerPrompt.toLowerCase()
 const computerSelection = computerPlay();
-alert(playRound(playerSelection, computerSelection));
+playerSelection
+alert(playRound(playerSelection[0]
+  .toUpperCase() + '' + playerSelection
+  .slice(1), computerSelection[0]
+  .toUpperCase() + '' + computerSelection
+  .slice(1)));
