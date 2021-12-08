f9eb415543cea6e8cb7e39871e0980ba67d10579
EgorKravetz <kravetzegor@gmail.com>
2021-12-08 11:45:40 +0300

    com

--- script.js
+++ script.js
@@ -1,53 +1,34 @@
 const computerPlay = function() {
-  const arr = ['rock','paper','scissors'];
+  const arr = ['Rock','Paper','Scissors'];
   return arr[Math.floor(Math.random() * 3)];
 }
-
-
 
 const playRound = function(playerSelection, computerSelection) {
   if (playerSelection == null) {
     return `You canceled game`}
-  else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    return `You win, ${playerSelection[0]
-      .toUpperCase() + '' + playerSelection
-      .slice(1)} beat ${computerSelection[0]
-      .toUpperCase() + '' + computerSelection
-      .slice(1)}`}
-  else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    return `You win, ${playerSelection[0]
-      .toUpperCase() + '' + playerSelection
-      .slice(1)} beat ${computerSelection[0]
-      .toUpperCase() + '' + computerSelection
-      .slice(1)}`}
-  else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    return `You win, ${playerSelection[0]
-      .toUpperCase() + '' + playerSelection
-      .slice(1)} beat ${computerSelection[0]
-      .toUpperCase() + '' + computerSelection
-      .slice(1)}`}
+
+  else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    return `You win, ${playerSelection} beat ${computerSelection}`
+  }
+  else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+    return `You win, ${playerSelection} beat ${computerSelection}`
+  }
+  else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+    return `You win, ${playerSelection} beat ${computerSelection}`
+  }
   else if (playerSelection == computerSelection) {
-    return `Draw ps ${playerSelection[0]
-      .toUpperCase() + '' + playerSelection
-      .slice(1)} and cs ${computerSelection[0]
-      .toUpperCase() + '' + computerSelection
-      .slice(1)}`}
+    return `Draw ps ${playerSelection} and cs ${computerSelection}`
+  }
     else {
-      return `You lose, ${computerSelection[0]
-      .toUpperCase() + '' + computerSelection
-      .slice(1)} beat ${playerSelection[0]
-        .toUpperCase() + '' + playerSelection
-        .slice(1)}`;
+      return `You lose, ${computerSelection} beat ${playerSelection}`;
      }
    }
 
    const playerSelection = prompt('Enter word');
-/*
-
-   //const playerSelection = playerPrompt.toLowerCase()
-
-   const playerSelection = null? "You canceled game" : playerPrompt.toLowerCase();
-*/
    const computerSelection = computerPlay();
 
 alert(playRound(playerSelection, computerSelection));
+
+const container = document.createElement('div');
+container.textContent = 'Div';
+document.body.appendChild('container');
