b700e94e89d39cf24ba1d370a44e407abc59dac8
EgorKravetz <kravetzegor@gmail.com>
2022-01-13 14:29:42 +0300

    finaly completed js part

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
+const buttonRock = document.createElement('button');
+const buttonPaper = document.createElement('button');
+const buttonScissors = document.createElement('button');
+
+const section = document.createElement('section');
+
+
+buttonRock.textContent = 'Rock';
+buttonPaper.textContent = 'Paper';
+buttonScissors.textContent = 'Scissors';
+
+buttonRock.className = 'btn';
+buttonPaper.className = 'btn';
+buttonScissors.className = 'btn';
+
+
+document.body.append(buttonRock)
+document.body.append(buttonPaper)
+document.body.append(buttonScissors)
+
 const computerPlay = function() {
   const arr = ['Rock','Paper','Scissors'];
   return arr[Math.floor(Math.random() * 3)];
-}
+  }
+
+  const buttons = document.querySelectorAll('.btn');
+
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+    playRound(this.textContent, computerPlay());
+    })
+  })
 
 const playRound = function(playerSelection, computerSelection) {
-  if (playerSelection == null) {
-    return `You canceled game`}
-
-  else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-    return `You win, ${playerSelection} beat ${computerSelection}`
+  
+  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+   return console.log(`You win, ${playerSelection} beat ${computerSelection}`)
+ }
+ else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+   return console.log(`You win, ${playerSelection} beat ${computerSelection}`)
+ }
+ else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+   return console.log(`You win, ${playerSelection} beat ${computerSelection}`)
+ }
+ else if (playerSelection == computerSelection) {
+   return console.log(`Draw ps ${playerSelection} and cs ${computerSelection}`)
+ }
+   else {
+     return console.log(`You lose, ${computerSelection} beat ${playerSelection}`);
+    }
   }
-  else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-    return `You win, ${playerSelection} beat ${computerSelection}`
-  }
-  else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-    return `You win, ${playerSelection} beat ${computerSelection}`
-  }
-  else if (playerSelection == computerSelection) {
-    return `Draw ps ${playerSelection} and cs ${computerSelection}`
-  }
-    else {
-      return `You lose, ${computerSelection} beat ${playerSelection}`;
-     }
-   }
+ 
 
-   const playerSelection = prompt('Enter word');
-   const computerSelection = computerPlay();
-
-alert(playRound(playerSelection, computerSelection));
-
-const container = document.createElement('div');
-container.textContent = 'Div';
-document.body.appendChild('container');
-document.container.textContent('DIV')
-
-document.body.container.style('color: red');
