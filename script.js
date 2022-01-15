1417b4af17550351140eb398395b45669e825f78
EgorKravetz <kravetzegor@gmail.com>
2022-01-15 13:34:32 +0300

    completed updateScore

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const buttonRock = document.createElement('button');
 const buttonPaper = document.createElement('button');
 const buttonScissors = document.createElement('button');
 
-const section = document.createElement('section');
-
-
 buttonRock.textContent = 'Rock';
 buttonPaper.textContent = 'Paper';
 buttonScissors.textContent = 'Scissors';
@@ -13,42 +10,72 @@ buttonRock.className = 'btn';
 buttonPaper.className = 'btn';
 buttonScissors.className = 'btn';
 
+const divForBtn = document.querySelector('.buttons.container')
+let display = document.querySelector('.img.items.text');
 
-document.body.append(buttonRock)
-document.body.append(buttonPaper)
-document.body.append(buttonScissors)
+divForBtn.append(buttonRock);
+divForBtn.append(buttonPaper);
+divForBtn.append(buttonScissors);
 
+let pScore = 0;
+let cScore = 0;
+
+let playerImage = document.querySelector('.img.items.player');
+let compImage = document.querySelector('.img.items.comp');
+let playerImageSrc = playerImage.getAttribute('src');
+let compImageSrc = compImage.getAttribute('src');
+      
 const computerPlay = function() {
   const arr = ['Rock','Paper','Scissors'];
   return arr[Math.floor(Math.random() * 3)];
   }
 
   const buttons = document.querySelectorAll('.btn');
-
-
+  const updateScore = () => { 
+    let scorePlayer = document.querySelector('.score.items.player');
+    let scoreComp = document.querySelector('.score.items.comp');
+    scorePlayer.textContent = pScore;
+    scoreComp.textContent = cScore;
+  }
+    
   buttons.forEach(button => {
     button.addEventListener('click', function() {
-    playRound(this.textContent, computerPlay());
-    })
+      const computerSelection = computerPlay();
+      playRound(this.textContent, computerSelection);
+        playerImage.setAttribute('src', `./assets/png/${this.textContent}.png`);
+        compImage.setAttribute('src', `./assets/png/${computerSelection}.png`);
+        playerImage.setAttribute('src', `./assets/png/${this.textContent}.png`);
+        compImage.setAttribute('src', `./assets/png/${computerSelection}.png`);
   })
+})
 
-const playRound = function(playerSelection, computerSelection) {
-  
-  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-   return console.log(`You win, ${playerSelection} beat ${computerSelection}`)
- }
- else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-   return console.log(`You win, ${playerSelection} beat ${computerSelection}`)
- }
- else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-   return console.log(`You win, ${playerSelection} beat ${computerSelection}`)
- }
- else if (playerSelection == computerSelection) {
-   return console.log(`Draw ps ${playerSelection} and cs ${computerSelection}`)
- }
-   else {
-     return console.log(`You lose, ${computerSelection} beat ${playerSelection}`);
-    }
-  }
- 
+      const playRound = function(playerSelection, computerSelection) {
 
+        if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+          display.textContent = `You win, ${playerSelection} beat ${computerSelection}`;
+          pScore++;
+          updateScore();
+          return
+          
+       } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        display.textContent = `You win, ${playerSelection} beat ${computerSelection}`;
+         pScore++;
+         updateScore();
+         return
+
+       } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+         display.textContent = `You win, ${playerSelection} beat ${computerSelection}`
+         pScore++;
+         updateScore();
+         return
+
+       } else if (playerSelection == computerSelection) {
+         return display.textContent = `Draw ps ${playerSelection} and cs ${computerSelection}`;
+
+       } else {
+           display.textContent = `You lose, ${computerSelection} beat ${playerSelection}`;
+           cScore++;
+           updateScore();
+           return
+          }
+        }
